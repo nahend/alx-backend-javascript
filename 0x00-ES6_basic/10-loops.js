71a3625325527c165b63db33f2c3a4971a6f864a
nahend <101421896+nahend@users.noreply.github.com>
2023-01-26 16:45:28 -0500

    commit

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const returnArray = [];
+  for (const element of array) {
+    returnArray.push(appendString + element);
+  }
+  return returnArray;
+}
